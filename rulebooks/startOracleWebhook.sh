29554b8ea7a4985d5c3a3c5276fef6e4204b4325
BharathKandala <61424631+BharathKandala@users.noreply.github.com>
2023-12-15 15:56:40 +0530

    Add files via upload

---
+++ rulebooks/startOracleWebhook.sh
@@ -0,0 +1,1 @@
+curl -H 'Content-Type: application/json' -d '{"message": "Start Oracle"}' 127.0.0.1:8080/webhook
---
+++ rulebooks/stopOracleWebhook.sh
@@ -0,0 +1,1 @@
+curl -H 'Content-Type: application/json' -d '{"message": "Stop Oracle"}' 127.0.0.1:8080/webhook
